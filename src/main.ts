4d796a55ee53c79c49e5f3798e34be0da7eb8bb4
JoeHughes9877 <joeydarlo1000@gmail.com>
2025-11-03 14:09:30 +0000

    get all authors infomation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 createApp(App).mount('#app')
+
+const API_URL = 'https://cdex.joe-hughes.dev';
+const AUTH_KEY = 'PmLKYSrMjKDVS5XzqC3lxQ';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Authorization: `Bearer ${AUTH_KEY}`,
+  },
+});
+
+export async function fetchAllData() {
+  const res = await api.get('/authors/');
+  return res.data;
+}
